7d0a3df0e81b9e401673fc29b2cf9b06c89b3b16
zeon77 <zeon77@gmail.com>
2021-06-17 11:20:39 +0200

    4. feladat

--- EU/Program.cs
+++ EU/Program.cs
@@ -17,6 +17,9 @@ namespace EU
 
             //3. feladat
             Console.WriteLine($"3. feladat: Az EU tagállamainak száma: {tagállamok.Count} db");
+
+            //4. feladat
+            Console.WriteLine($"4. feladat: 2007-ben {tagállamok.Where(x => x.CsatlakozásDátuma.Year == 2007).Count()} ország csatlakozott.");
         }
     }
 }
